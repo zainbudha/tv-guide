0004f81eb0bb5245df8b7b8683a5462e79f79a33
Ankit Jain <zainbudha@gmail.com>
2016-01-08 23:12:16 +0530

    Updating Karma configuration file to point to guide folder

---
+++ app/guide/guide_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('tvGuide.guide module', function() {
+
+  beforeEach(module('tvGuide.guide'));
+
+  describe('guide controller', function(){
+  	
+    it('should ....', inject(function($controller) {
+      //spec body
+      var guideCtrl = $controller('GuideController');
+      expect(guideCtrl).toBeDefined();
+    }));
+
+  });
+});
