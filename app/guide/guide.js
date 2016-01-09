e4198d75e4bfe2d6952c115b40719917b68133e4
Ankit Jain <zainbudha@gmail.com>
2016-01-09 08:15:45 +0530

    Displaying Name and Airtime of all the events in today's schedule using the ng-repeat directive. Karma test added to verify the schedule in the controller.

--- app/guide/guide.js
+++ app/guide/guide.js
@@ -1,16 +1,23 @@
 'use strict';
 
-angular.module('tvGuide.guide', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/guide', {
-    templateUrl: 'guide/guide.html',
-    controller: 'GuideController',
-    controllerAs: 'guideCtrl',
-    resolve: {
-          schedule: function(req) {
-            return req.getDaySchedule();
+angular.module('tvGuide.guide', ['ngRoute', 'tvGuide.comm'])
+.config(['$routeProvider', 
+  function($routeProvider) {
+    $routeProvider.when('/guide', {
+      templateUrl: 'guide/guide.html',
+      controller: 'GuideController',
+      controllerAs: 'guideCtrl',
+      resolve: {
+            schedule: function(req) {
+              return req.getDaySchedule();
+            }
           }
-        }
-  });
+    });
 }])
+
+.controller('GuideController',['$scope', 'schedule',
+function($scope, schedule) {
+    var guide = this;
+
+    guide.schedule = schedule;
+}]);
--- app/guide/guide_test.js
+++ app/guide/guide_test.js
@@ -5,12 +5,21 @@ describe('tvGuide.guide module', function() {
   beforeEach(module('tvGuide.guide'));
 
   describe('guide controller', function(){
-  	
-    it('should ....', inject(function($controller) {
-      //spec body
-      var guideCtrl = $controller('GuideController');
-      expect(guideCtrl).toBeDefined();
-    }));
 
+  	var $scope, schedule, guideCtrl;
+
+    beforeEach(inject(function($controller) {
+      $scope = {};
+      schedule = {};
+      guideCtrl = $controller('GuideController', {$scope : $scope, schedule:schedule});
+    }));
+  	
+    it('should be defined', function() {
+      expect(guideCtrl).toBeDefined();
+    });
+
+	it('schedue should be defined', function() {
+      expect(schedule).toBeDefined();
+    });
   });
 });
