2f97f4f18fc53261e3a1bef83ff1360579805a2e
anjain4 <anjain4@cisco.com>
2016-01-22 23:24:28 +0530

    Added controller unit test for the GuideController.

--- app/guide/guide_test.js
+++ app/guide/guide_test.js
@@ -2,14 +2,36 @@
 
 describe('tvGuide.guide module', function() {
 
+  beforeEach(angular.mock.module('tvGuide.model'));
   beforeEach(angular.mock.module('tvGuide.guide'));
 
   describe('guide controller', function(){
 
-  	var GuideController = {};
-  	beforeEach(angular.mock.inject(function(_GuideController_) {
-  		GuideController = _GuideController_;
+  	var $controller;
+  	var $q;
+  	var $rootScope;
+  	var req;
+  	var channels;
+  	var $this;
+
+  	beforeEach(angular.mock.inject(function(_$controller_, _$q_, _$rootScope_, _req_) {
+  		$controller = _$controller_;
+  		$q = _$q_;
+  		$rootScope = _$rootScope_;
+  		req = _req_;
   	}))
+
+  	it("should test the channels", function() {
+  		spyOn(req, 'getDaySchedule').and.callFake(function() {
+  			var deferred = $q.defer();
+  			deferred.resolve("a");
+  			return deferred.promise;
+  		})
+
+  		$this = $controller("GuideController", { req: req });
+  		$rootScope.$apply();
+  		expect($this.channels).toBe("a");
+  	})
 
   });
 });
